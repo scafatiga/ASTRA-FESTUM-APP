6ea41ed75f2cb92dfcddf4e1a7842db0fa90a25c
scafatiga <gabrielscafati@yahoo.com>
2026-09-02 18:01:32 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-const express = require('express');
-const { Pool } = require('pg');
-const path = require('path');
+import express from 'express';
+import pkg from 'pg';
+const { Pool } = pkg;
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 app.use(express.json());
