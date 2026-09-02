dfc9c7dc8ffae11afdadbfaf14b2822c20407792
scafatiga <gabrielscafati@yahoo.com>
2026-09-02 18:06:54 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 import pkg from 'pg';
 const { Pool } = pkg;
 import path from 'path';
 import { fileURLToPath } from 'url';
+
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname)));
