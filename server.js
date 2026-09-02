c783020e6a517d8e686c304b45dfe895202d7d15
Gabriel Scafati <gabrielscafati@gmail.com>
2026-09-02 09:10:04 +0200

    fix: compatibilidad de modulos Node.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-import express from 'express';
-import { createClient } from '@supabase/supabase-js';
-import dotenv from 'dotenv';
-dotenv.config();
+const express = require('express');
+const { createClient } = require('@supabase/supabase-js');
+require('dotenv').config();
 
 const app = express();
 app.use(express.json());
